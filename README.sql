4e2a964f1c00a98aa655054c7f12c7363bd89e7b
S.Mehran M.Ziabary <ziabary@targoman.com>
2019-04-06 12:54:48 +0430

    README file added

---
+++ README.sql
@@ -0,0 +1,7 @@
+#Pacman DB
+
+This repo includes all the tables related to Pacman game offered by Tarjomyar and other 9 Startups in nowrooz 98 (March/April 2019). These tables includes all the data excluding just user emails.
+
+This database has been provided in order to allow users to check logs and find any fraudant plays.
+
+
